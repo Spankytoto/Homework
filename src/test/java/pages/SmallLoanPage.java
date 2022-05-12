0de2266c930510b54d2f5a2ecb4fdd1188dee90e
Artur <baker306@bk.ru>
2022-05-12 17:05:52 +0300

    Add parallel

--- src/test/java/pages/SmallLoanPage.java
+++ src/test/java/pages/SmallLoanPage.java
@@ -1,7 +1,6 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
-import io.qameta.allure.Step;
 import org.openqa.selenium.support.FindBy;
 
 import static com.codeborne.selenide.Selenide.actions;
