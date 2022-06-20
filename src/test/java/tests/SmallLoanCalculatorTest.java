cf471742ce8d60adce7e44acc731fad69cbf23fa
Artur <baker306@bk.ru>
2022-06-20 15:25:18 +0300

    fix

--- src/test/java/tests/SmallLoanCalculatorTest.java
+++ src/test/java/tests/SmallLoanCalculatorTest.java
@@ -2,7 +2,6 @@ package tests;
 
 import core.Retry;
 import core.Settings;
-import helper.RandomHelper;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.testng.annotations.BeforeMethod;
@@ -19,16 +18,14 @@ public class SmallLoanCalculatorTest extends Settings {
 
     @Test(description = "Test-case #1: Check that “Choose loan sum” slider works correctly", groups = "Small Loan Calculator", retryAnalyzer = Retry.class)
     public void checkChooseLoanSumSlider() {
-        smallLoanCalculatorSteps.changeLoanSumSliderPosition(RandomHelper.getRandomNumber(50, 500));
+        smallLoanCalculatorSteps.changeLoanSumSliderPosition(500);
         smallLoanCalculatorSteps.compareSumOverSliderAndInField();
     }
 
     @Test(description = "Test-case #2: Check that you cannot type more than 20 000 in “Choose loan sum” field", groups = "Small Loan Calculator", retryAnalyzer = Retry.class)
     public void checkLoanSumFieldCapacity() {
-        Integer sum = RandomHelper.getRandomNumber(21000, 100000);
-
-        smallLoanCalculatorSteps.fillLoanSum(sum);
-        smallLoanCalculatorSteps.checkMaxValueInLoanSumField(sum);
+        smallLoanCalculatorSteps.fillLoanSum(21000);
+        smallLoanCalculatorSteps.checkMaxValueInLoanSumField(21000);
     }
 
     @Test(description = "Test-case #3: Check that you cannot get 0 Euro for loan", groups = "Small Loan Calculator", retryAnalyzer = Retry.class)
@@ -39,10 +36,9 @@ public class SmallLoanCalculatorTest extends Settings {
 
     @Test(description = "Test-case #4: Check that monthly payment changes when you change loan sum", groups = "Small Loan Calculator", retryAnalyzer = Retry.class)
     public void checkThatMonthlyPaymentChanges() {
-        Integer sum = RandomHelper.getRandomNumber(300, 20000);
         Integer beforeAction = smallLoanCalculatorSteps.getMonthlyPaymentSum();
 
-        smallLoanCalculatorSteps.fillLoanSum(sum);
+        smallLoanCalculatorSteps.fillLoanSum(20000);
 
         Integer afterAction = smallLoanCalculatorSteps.getMonthlyPaymentSum();
 
@@ -53,7 +49,7 @@ public class SmallLoanCalculatorTest extends Settings {
     public void checkThatLoanTermSliderWorks() {
         Integer beforeAction = smallLoanCalculatorSteps.getMonthlyPaymentSum();
 
-        smallLoanCalculatorSteps.changeLoanTermSliderPosition(RandomHelper.getRandomNumber(50, 500));
+        smallLoanCalculatorSteps.changeLoanTermSliderPosition(500);
 
         Integer afterAction = smallLoanCalculatorSteps.getMonthlyPaymentSum();
 
@@ -62,10 +58,9 @@ public class SmallLoanCalculatorTest extends Settings {
 
     @Test(description = "Test-case #6: Check that “Agreement fee” changes correctly", groups = "Small Loan Calculator", retryAnalyzer = Retry.class)
     public void checkThatAgreementFeeChanges() {
-        Integer sum = RandomHelper.getRandomNumber(300, 20000);
         Integer beforeAction = smallLoanCalculatorSteps.getAgreementFeeValue();
 
-        smallLoanCalculatorSteps.fillLoanSum(sum);
+        smallLoanCalculatorSteps.fillLoanSum(20000);
 
         Integer afterAction = smallLoanCalculatorSteps.getAgreementFeeValue();
 
@@ -74,13 +69,12 @@ public class SmallLoanCalculatorTest extends Settings {
 
     @Test(description = "Test-case #7: Check that slider moves when you change loan sum manually", groups = "Small Loan Calculator", retryAnalyzer = Retry.class)
     public void checkSumOverSliderAfterMove() {
-        Integer sum = RandomHelper.getRandomNumber(300, 20000);
         Integer sumOverSliderBeforeAction = smallLoanCalculatorSteps.getLoanSumOverSlider();
 
-        smallLoanCalculatorSteps.fillLoanSum(sum);
+        smallLoanCalculatorSteps.fillLoanSum(20000);
 
         Integer sumOverSliderAfterAction = smallLoanCalculatorSteps.getLoanSumOverSlider();
 
-        smallLoanCalculatorSteps.checkValuesOverSlider(sumOverSliderBeforeAction, sumOverSliderAfterAction, sum);
+        smallLoanCalculatorSteps.checkValuesOverSlider(sumOverSliderBeforeAction, sumOverSliderAfterAction, 20000);
     }
 }
